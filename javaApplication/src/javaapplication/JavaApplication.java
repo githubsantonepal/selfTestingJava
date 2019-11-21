b7114c6757f33af013c9d12f60480c7fc14ef95b
santonepal@gmail.com <githubsantonepal@users.noreply.github.com>
2019-11-21 12:16:33 +0200

    testing

---
+++ javaApplication/src/javaapplication/JavaApplication.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+
+import java.util.Scanner;
+
+/**
+ 
+ * @author santoshji
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x=10;
+        //Scanner scannerObj= new Scanner(System.in);
+        System.out.println("The value assigned is "+x);// TODO code application logic here
+    }
+    
+}
